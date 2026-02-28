8df1d62be7e0c44e2bf01fd6f790248bb63954a3
Yousuf Ahmed H. Saleh <nv23026@nvtc.edu.bh>
2026-02-28 23:56:22 +0000

    create a python code for testing the app

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+def test_app_import():
+    from app import app
+    assert app is not None
+
+def test_home_status():
+    from app import app
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        response = client.get('/')
+    assert response.status_code in [200, 302]
