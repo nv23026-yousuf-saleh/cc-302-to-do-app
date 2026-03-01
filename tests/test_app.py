174b5a72ef6414c88cb885b995a74d1a6f28bcb7
Yousuf Ahmed H. Saleh <nv23026@nvtc.edu.bh>
2026-03-01 00:32:22 +0000

    rewrite python code

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,13 @@ def test_app_import():
     from app import app
     assert app is not None
 
+
 def test_home_status():
     from app import app
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
 
     with app.test_client() as client:
-        response = client.get('/')
+        response = client.get("/")
+
     assert response.status_code in [200, 302]
+
